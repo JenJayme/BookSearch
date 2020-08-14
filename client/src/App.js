57c9d362dfe03cccb519438b1536fa0b19a991af
Royce Williams <roycejames1130@gmail.com>
2020-08-14 11:37:37 -0700

    fixed all of the issues happening on the back end

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
-import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h2>Welcome to React</h2>
       </div>
       <p className="App-intro">
