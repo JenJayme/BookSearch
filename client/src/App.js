95e7bc5d198f3bc7c0df630716733ba99c9784f5
Alex Aguirrebena <alex.aguirrebena@gmail.com>
2020-08-14 11:44:11 -0700

    App.js complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from "react";
-import "./App.css";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Jumbotron from "./components/Jumbotron";
+import NavBar from "./components/Nav";
+import Books from "./pages/Books";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
+    <Router>
+      <div>
+        <NavBar/>
+        <Jumbotron />
+        <Switch>
+          <Route exact path="/" component={Books}/>
+          <Route exact path="/saved" component={Books}/>
+        </Switch>
       </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
-  );
+    </Router>
+  )
 }
-
 
 export default App;
