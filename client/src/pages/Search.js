c829dd97b5d6882696549cbb4a06af7ab87afc19
Jen Jayme <jeniferjayme@gmail.com>
2020-08-18 14:42:59 -0700

    Added app.css with link in index.js for custom styles.

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -4,11 +4,26 @@ import React from 'react';
 import NavBar from '../components/navbar';
 import MyJumbotron from '../components/jumbotron';
 import Results from '../components/results';
+// import SearchBar from "../components/searchBar"
+import Container from "react-bootstrap/Container"
+
+// export default function SearchPage() {
+//     return (
+//         <div>
+//             <h3>Search Page</h3>
+//             <Results />
+//             {/* <p>Book Details here</p> */}
+//         </div>
+//     )
+// }
 
 export default function SearchPage() {
     return (
         <div>
+            <Container>
             <h3>Search Page</h3>
+                {/* <SearchBar/> */}
+                </Container>
             <Results />
             {/* <p>Book Details here</p> */}
         </div>
