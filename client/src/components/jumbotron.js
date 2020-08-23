a10cd03fd858b3e0c9a52f9285937734119611b2
Jen Jayme <jeniferjayme@gmail.com>
2020-08-22 17:55:00 -0700

    Added some styling pre-deploybefore re-deploying

--- client/src/components/jumbotron.js
+++ client/src/components/jumbotron.js
@@ -5,12 +5,17 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 function MyJumbotron() {
 
   return (
-    <Jumbotron>
-      <div className="bannerImg"></div>
-      {/* <img src="./books.jpg" alt="wall of antique books"></img> */}
-      <h1 className="text-center">React Google Search</h1>
-      <h4 className="text-center"> Search for and Save Books of Interest.</h4>
-    </Jumbotron>
+    <div>
+      <Jumbotron className="heroImg">
+
+      <div className="heroTxt">
+      <h1>Book Search</h1>
+      <h4> Search for and Save Books of Interest </h4>
+      <p>powered by React and Google Books API</p>
+      </div>
+
+      </Jumbotron>
+    </div>
   )
 }
 export default MyJumbotron;
